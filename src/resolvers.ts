19c2c75856a42cd91ff2b002ef5c2e616cac97e6
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-06-12 20:40:02 +0900

    5

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -6,7 +6,11 @@ import * as bcrypt from 'bcryptjs';
 
 export const resolvers: IResolvers = {
 	Query: {
-		hello: () => 'hi',
+		me: (_, __, {req}) => {
+			if (!req.session.userId) {
+			return null
+		}
+		return User.findOne(req.session.userId)
 	},
 	Mutation: {
 		register: async (_, { email, password }) => {
@@ -24,6 +28,8 @@ export const resolvers: IResolvers = {
 			if (!valid) {
 				return null;
 			}
+
+			req.session.userId = user.id;
 
 			return user;
 		},
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -9,7 +9,7 @@ export const typeDefs = gql`
 	}
 
 	type Query {
-		hello: String!
+		me: User
 	}
 
 	type Mutation {
